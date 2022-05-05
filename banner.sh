bec1e55734a758ac3bc11c9ad717bb4d222f9af8
smithwhitten <02whitten.smith@gulllakecs.org>
2022-05-05 12:10:24 -0400

    banner iteration

---
+++ banner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+banner()
+{
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`date`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+}
+
+banner "Starting the Job"
+sleep 3
+
+banner "Copying files"
+sleep 4
+
+banner "Finished."
+
+banner "Finished."
