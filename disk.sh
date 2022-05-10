55b5b0a6fc23b6ff36c23306938f5735e992f156
smithwhitten <02whitten.smith@gulllakecs.org>
2022-05-10 13:00:42 -0400

    added disk usage .sh

---
+++ disk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name: disk_usage.sh
+# Description: Finds total disk usage on the machine.
+# Usage: disk_usage.sh
+# Example: ./disk_usage.sh
+
+
+# TODO: add option to supply a list of disk name patterns
+
+main() {
+
+    # get file system names and percentage of use
+    result=$(df -h | grep sda | awk '{ print $1 " " $5 }' | cut -d'%' -f1)
+
+    echo -e "$result"
+
+}
+
+main "$@"
